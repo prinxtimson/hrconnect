dd04323b6ae415c74985a3cd2460b2c87cc4eb81
Babajide Timson <timson.babajide@gmail.com>
2026-03-04 14:31:31 +0100

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_BASE_PATH || "http://localhost:8080",
+        target: import.meta.env.VITE_API_URL || "http://localhost:8080",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
