2cfb957be8eece77f8b3d10bc4cf360539e285e3
Babajide Timson <timson.babajide@gmail.com>
2026-03-08 20:33:14 +0100

    log trigger event

--- appwrite_functions/main.js
+++ appwrite_functions/main.js
@@ -22,7 +22,7 @@ export default async ({ req, res, log, error }) => {
       },
     });
 
-    log(`Profile created for user: ${user.$id}`);
+    log(`Profile created for user: ${process.env.APPWRITE_FUNCTION_EVENT}`);
     return res.send("Profile created");
   } catch (err) {
     error("Failed to create: " + err.message);
