5043983b18f9ffd6c1d2d913455e936a123f1f5e
Babajide Timson <timson.babajide@gmail.com>
2026-03-09 01:27:35 +0100

    update appwrite unction

--- appwrite_functions/main.js
+++ appwrite_functions/main.js
@@ -25,35 +25,6 @@ export default async ({ req, res, log, error }) => {
       });
     }
 
-    if (
-      req.headers["x-appwrite-event"] ==
-        `databases.${databaseId}.tables.leaveapplications.rows.${payload.$id}.update` &&
-      payload.status == "approved"
-    ) {
-      const days = calculateDays(payload.startDate, payload.endDate);
-
-      const leaveBalances = await databases.listDocuments({
-        databaseId: databaseId,
-        collectionId: "leavebalances",
-        queries: [
-          Query.equal("user.$id", payload.user), // Specify which rows to update
-          Query.equal("leaveType", payload.leaveType),
-        ],
-      });
-
-      leaveBalances.documents.forEach(async (doc) => {
-        await databases.updateDocument({
-          databaseId: process.env.APPWRITE_DATABASE_ID,
-          collectionId: "leavebalances",
-          documentId: doc.$id,
-          data: {
-            usedDays: doc.usedDays + days,
-            balanceDays: doc.balanceDays - days,
-          },
-        });
-      });
-    }
-
     log(`Profile created for user: ${req.headers["x-appwrite-event"]}`);
     return res.send("Profile created");
   } catch (err) {
@@ -61,11 +32,3 @@ export default async ({ req, res, log, error }) => {
     return res.send("Failed to create profile", 500);
   }
 };
-
-function calculateDays(startDate, endDate) {
-  let start = new Date(startDate);
-  let end = new Date(endDate);
-  let timeDifference = end - start;
-  let daysDifference = timeDifference / (1000 * 3600 * 24);
-  return daysDifference;
-}
